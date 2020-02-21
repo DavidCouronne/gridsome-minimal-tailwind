652f6cef27b7eec68ca6996aacb4c79dfe128002
David Couronné <couronne.david@gmail.com>
2020-02-21 06:02:27 +0100

    layout(): create 3 colums layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,20 @@
 <template>
   <!-- Read how Layouts, Pages, Templates and collections work here: https://gridsome.org/docs/core-concepts/ -->
-  <div class="container max-w-3xl px-4 mx-auto">
-    <slot />
+  <div>
+    <main>
+        <nav class="bg-blue-500 h-16"></nav>
+        <section class="bg-pink-500 fixed top-16 h-screen">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo iure non quas, provident debitis magnam aliquam similique veniam reiciendis aperiam rerum suscipit libero, quam tempore praesentium placeat impedit nulla. Unde.
+
+        </section>
+        <article>
+          <div class="content bg-yellow-500 mt-16">
+           <slot />
+          </div>
+        </article>
+      </main>
+    
   </div>
 </template>
 
@@ -10,3 +23,41 @@ export default {
   components: {}
 }
 </script>
+
+<style>
+
+  
+  nav {
+    
+   
+    z-index: 9;
+    width: 100%;
+    position: fixed;
+  }
+  
+  article {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 20vw auto;
+  }
+  
+  article p {
+    padding-bottom: 1500px;
+  }
+  
+  section {
+    
+    width: 20vw;
+    
+    
+    
+    overflow-y: scroll;
+  }
+  
+  .content {
+    
+    position: relative;
+    grid-column-start: 2;
+    
+  }
+</style>
